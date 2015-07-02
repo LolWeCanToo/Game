89e5b711dd90d0c235cb500b7c06913904e9e6a5
DrWho <for-temp@mail.ru>
2015-07-02 14:20:24 +0400

    Minecraft-Robocraft

---
+++ The Game/Assets/scripts/SectorsCreate.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SectorsCreate : MonoBehaviour {
+	float BlockX, BlockY, BlockZ, sectors; 
+	public GameObject obj;
+	GameObject enemy;
+	public void CreateSectors(Transform world, Vector3 blockworld, int sectors)
+	{
+		BlockX = (int)blockworld.x;
+		BlockY = (int)blockworld.y;
+		BlockZ = (int)blockworld.z;
+		this.sectors = sectors;
+		float Xpos = ((BlockX / sectors) / 2) - 0.25f;//, ypos
+		for (int i=0; i<sectors; i++) {
+			float Ypos = ((BlockY / sectors) / 2) - 0.25f;//, ypos
+			for (int j=0; j<sectors; j++) {
+				float Zpos = ((BlockZ / sectors) / 2) - 0.25f;//, ypos
+				for (int k=0; k<sectors; k++) {
+					/*GameObject sector = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					sector.name = "" + (i+1)+"-"+(j+1)+"-"+(k+1);
+					sector.transform.parent=world;
+					sector.transform.position = new Vector3(Xpos,Ypos,Zpos);
+*/
+						Instantiate(obj, new Vector3(Xpos,Ypos,Zpos), Quaternion.identity);
+					Zpos+=(BlockZ/sectors);
+				}
+				Ypos+=(BlockY/sectors);
+			}
+			Xpos+=(BlockX/sectors);
+		}
+	}
+	public void CreateBlocks(Transform world, Vector3 blockworld, int sectors)
+	{
+		BlockX = (int)blockworld.x;
+		BlockY = (int)blockworld.y;
+		BlockZ = (int)blockworld.z;
+		sectors = 1;
+		float Xpos = ((BlockX / sectors) / 2);
+		float Ypos = 0f+0.25f;//((BlockY / sectors) / 2) - 1;
+		float Zpos = ((BlockZ / sectors) / 2);
+		Instantiate(obj, new Vector3(Xpos,Ypos,Zpos), Quaternion.identity);
+		for (int i = 0; i<10; i++) {
+			Zpos-=0.5f;
+			enemy = (GameObject)Instantiate(obj, new Vector3(Xpos,Ypos,Zpos), Quaternion.identity);
+			enemy.name = "block"+nextNameNumber;
+			nextNameNumber++;
+			//Instantiate(obj, new Vector3(Xpos,Ypos,Zpos), Quaternion.identity);
+		}
+	}
+	int nextNameNumber = 0;
+	public bool CreateBlock(Vector3 hpos, Vector3 hsize)
+	{
+		RaycastHit hit;
+		if (Physics.Raycast (Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0)), out hit, 80)) {
+			if (hit.collider.gameObject == gameObject || hit.collider.gameObject.tag=="building") 
+			{
+				//print(hit.collider.gameObject.tag);
+				if (hit.collider.gameObject.tag=="building") {
+					Vector3 pos = hit.collider.transform.position;
+					pos += hit.normal / 2;
+					if (Vector3.Distance (this.transform.position, pos) > 0) {
+						enemy = (GameObject)Instantiate (obj, pos, Quaternion.identity);
+						enemy.name = "block" + nextNameNumber;
+						nextNameNumber++;
+						return true;
+					}
+				} else {
+					Vector3 pos = transform.InverseTransformPoint (hit.point);//.collider.transform.position;
+					if(hit.collider.gameObject.GetComponent<MeshFilter>())
+					{
+						pos.y += 0.25f;
+						if(pos.x<0)pos.x -= 0.25f;
+						else pos.x += 0.25f;
+						if(pos.z<0)pos.z -= 0.25f;
+						else pos.z += 0.25f;
+						float Xpos2 = (pos.x) % 0.5f;
+						pos.x -= Xpos2;
+						float Zpos2 = (pos.z) % 0.5f;
+						pos.z -= Zpos2;
+						
+						pos.x += hpos.x;
+						pos.y += hpos.y;
+						pos.z += hpos.z;
+					}
+					else if(hit.collider.gameObject.GetComponent<Terrain>())
+					{
+					pos.y += 0.25f;
+					pos.x += 0.25f;
+					pos.z += 0.25f;
+					float Xpos2 = (pos.x) % 0.5f;
+					pos.x -= Xpos2;
+					float Zpos2 = (pos.z) % 0.5f;
+					pos.z -= Zpos2;
+
+					pos.x += hpos.x;
+					pos.y += hpos.y;
+					pos.z += hpos.z;
+					}
+					//print(pos.x+";"+pos.y+";"+pos.z);
+				//	print(hpos.x+";"+hpos.y+";"+hpos.z);
+
+					if (Vector3.Distance (this.transform.position, pos) > 0) {
+						enemy = (GameObject)Instantiate (obj, pos, Quaternion.identity);
+						enemy.name = "block" + nextNameNumber;
+						nextNameNumber++;
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+		public bool DestroyBlock()
+		{
+			RaycastHit hit;
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0)), out hit, 80)) {
+			if(hit.collider.gameObject!=gameObject)Destroy(hit.collider.gameObject);
+			}
+		return false;
+		}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ The Game/Assets/scripts/buildblocks.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildblocks : MonoBehaviour {
+	//public GameObject pl;
+	//public int numBlocks;
+	//private Plane plane;
+	public int sectorsamount;
+	float BlockX, BlockY, BlockZ;
+	float BlockXS, BlockYS, BlockZS;
+	SectorsCreate sectors;
+	// Use this for initialization
+	void Start () {
+		/*BlockX = (int)Terrain.activeTerrain.terrainData.size.x;
+		BlockY = (int)Terrain.activeTerrain.terrainData.size.y;
+		BlockZ = (int)Terrain.activeTerrain.terrainData.size.z;*/
+		BlockXS = (float)gameObject.GetComponent<Collider>().bounds.size.x;
+		BlockYS = (float)gameObject.GetComponent<Collider>().bounds.size.y;
+		BlockZS = (float)gameObject.GetComponent<Collider>().bounds.size.z;
+		BlockX = (float)gameObject.transform.position.x;
+		BlockY = (float)gameObject.transform.position.y;
+		BlockZ = (float)gameObject.transform.position.z;
+		//print(BlockX+";"+BlockY+";"+BlockZ);
+		sectors = gameObject.GetComponent<SectorsCreate> ();
+		//sectors.CreateBlocks (world.transform, new Vector3 (BlockX, BlockY, BlockZ), sectorsamount);
+	//	plane = (Plane)pl.GetComponent(Plane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	if (Input.GetButtonDown ("Fire1")) {
+			sectors.CreateBlock( new Vector3 (BlockX, BlockY, BlockZ),new Vector3 (BlockXS, BlockYS, BlockZS));
+		}
+		if (Input.GetButtonDown ("Fire2")) {
+			sectors.DestroyBlock();
+		}
+	}
+}
